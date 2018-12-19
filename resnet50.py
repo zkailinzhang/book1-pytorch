30ad5f44eea24985d38fb0df9a97fb14ba6b93d0
zkl99999 <klzhang1002@163.com>
2018-12-19 19:06:21 +0800

    updata

--- resnet50.py
+++ resnet50.py
@@ -16,5 +16,29 @@ from keras.preprocessing import image
 from keras.utils import layer_utils
 from keras.utils.data_utils import get_file
 
-from keras 
+from keras.applications import imagenet_utils
+
+def identity_block(input_tensor,kernel_size,filters,stage,block):
+
+    filter1,filter2,filter3 = filters
+    if K.image_data_format() == 'channel_last':
+        bn_axis = 3
+    else:
+        bn_axis =1
+    
+    conv_name_base = 'res' + str(stage)+block +'_branch'
+
+    bn_name_base = 'bn' + str(stage) + block +'_branch'
+
+    x = KL.Conv2D(filter1,(1,1),name=conv_name_base, '2a')(input_tensor)
+    x = KL.BatchNormalization(axis= bn_axis, name=bn_name_base,+'2a')(x)
+
+    
+
+
+
+
+
+
+
 
