9c346293cb6e27ccc5e564f59a9b329325969617
zkl99999 <klzhang1002@163.com>
2018-12-21 19:26:13 +0800

    update

--- resnet50.py
+++ resnet50.py
@@ -19,7 +19,9 @@ from keras.utils.data_utils import get_file
 from keras.applications import imagenet_utils
 
 def identity_block(input_tensor,kernel_size,filters,stage,block):
-
+    '''
+    heng
+    '''
     filter1,filter2,filter3 = filters
     if K.image_data_format() == 'channel_last':
         bn_axis = 3
@@ -32,6 +34,20 @@ def identity_block(input_tensor,kernel_size,filters,stage,block):
 
     x = KL.Conv2D(filter1,(1,1),name=conv_name_base, '2a')(input_tensor)
     x = KL.BatchNormalization(axis= bn_axis, name=bn_name_base,+'2a')(x)
+    x = KL.Activation('relu')(x)
+
+    x = KL.Conv2D(filter2,kernel_size,padding='same',name = conv_name_base+'2b')(x)
+    x = KL.BatchNormalization(axis=bn_axis,name=bn_name_base)(x)
+    x = KL.Activation('relu')(x)
+
+    x = KL.Conv2D(filter3,(1,1),name=conv_name_base+'2c')(x)
+    x = KL.BatchNormalization(axis=bn_axis,name=bn_name_base+'2c')(x)
+
+    x = keras.layers.add([x,input_tensor])
+    x = KL.Activation('relu')(x)
+
+    return x 
+
 
     
 
